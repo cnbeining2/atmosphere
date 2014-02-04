711c898df9ef373adc6092a890b9ebe78d98816a
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-02-04 15:37:19 -0700

    Gravatar component now accepts option type property

--- resources/js/cf3/components/common/gravatar.js
+++ resources/js/cf3/components/common/gravatar.js
@@ -5,22 +5,28 @@ define(['react', 'profile'], function(React, profile) {
             hash: React.PropTypes.string
         },
         getSrc: function(hash, icon_set) {
-            if (icon_set == 'default')
-                return "//www.gravatar.com/avatar/" + hash + "?d=identicon&s=50"; 
-            if (icon_set == 'unicorn')
-                return "//unicornify.appspot.com/avatar/" + hash + "?s=50";
-            if (icon_set == 'wavatar')
-                return "//www.gravatar.com/avatar/" + hash + "?d=wavatar&s=50";
-            if (icon_set == 'monster')
-                return "//www.gravatar.com/avatar/" + hash + "?d=monsterid&s=50";
-            if (icon_set == 'retro')
-                return "//www.gravatar.com/avatar/" + hash + "?d=retro&s=50";
-            if (icon_set == 'robot')
-                return "//robohash.org/" + hash + "?size=50x50";
-            return null;
+            switch (icon_set) {
+                case 'unicorn':
+                    return "//unicornify.appspot.com/avatar/" + hash + "?s=50";
+                case 'wavatar':
+                    return "//www.gravatar.com/avatar/" + hash + "?d=wavatar&s=50";
+                case 'monster':
+                    return "//www.gravatar.com/avatar/" + hash + "?d=monsterid&s=50";
+                case 'retro':
+                    return "//www.gravatar.com/avatar/" + hash + "?d=retro&s=50";
+                case 'robot':
+                    return "//robohash.org/" + hash + "?size=50x50";
+                default:
+                    return "//www.gravatar.com/avatar/" + hash + "?d=identicon&s=50"; 
+            }
         },
         render: function() {
-            var icon_set = profile.get('settings')['icon_set'];
+            /* If a type is specified in props, use it. Otherwise, use the 
+             * profile setting 
+             */
+            var icon_set = this.props.type;
+            if (!icon_set)
+                icon_set = profile.get('settings')['icon_set'];
             return React.DOM.img({src: this.getSrc(this.props.hash, icon_set)});
         }
     });
