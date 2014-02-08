caebda294166760ead0b7a602483d378998800b4
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-02-08 05:42:57 -0700

    Add application grid

--- resources/js/cf3/components/applications.js
+++ resources/js/cf3/components/applications.js
@@ -1,4 +1,38 @@
-define(['react', 'underscore', 'components/page_header', 'collections/applications'], function(React, _, PageHeader, Applications) {
+define(['react', 'underscore', 'components/page_header', 
+    'collections/applications', 'components/common/gravatar'], 
+    function(React, _, PageHeader, Applications, Gravatar) {
+
+    var Rating = React.createClass({
+        render: function() {
+            var repeatString = function(string, num) {
+                return new Array( num + 1 ).join( string );
+            };
+            return React.DOM.div({className: 'star-rating'}, 
+                repeatString("\u2605", this.props.rating) + 
+                repeatString("\u2606", 5 - this.props.rating));
+        }
+    });
+
+    var ApplicationPreview = React.createClass({
+        render: function() {
+            var app = this.props.application;
+
+            var icon;
+            if (app.get('icon'))
+                icon = React.DOM.img({
+                    src: app.get('icon'),
+                    width: 150,
+                    height: 150
+                });
+            else
+                icon = Gravatar({hash: app.get('uuid_hash'), size: 150});
+
+            return React.DOM.li({}, 
+                React.DOM.div({className: 'icon-container'}, icon),
+                React.DOM.div({className: 'app-name'}, app.get('name_or_id')),
+                Rating({rating: 4}));
+        }
+    });
 
     var ApplicationsHome = React.createClass({
         getInitialState: function() {
@@ -12,14 +46,12 @@ define(['react', 'underscore', 'components/page_header', 'collections/applicatio
         render: function() {
             var content = React.DOM.div({}, "loading");
             if (this.state.applications != null)
-                content = React.DOM.ul({}, this.state.applications.map(function(app) {
-                    if (!app.get('name'))
-                        console.log(app);
-                    return React.DOM.li({}, app.get('name_or_id')); 
+                content = React.DOM.ul({className: 'app-preview-list'}, this.state.applications.map(function(app) {
+                    return ApplicationPreview({application: app});
                 }));
 
             return React.DOM.div({},
-                PageHeader({title: 'Applications', helpText: this.helpText}),
+                PageHeader({title: 'Apps', helpText: this.helpText}),
                 content);
         },
         updateApplications: function(apps) {
