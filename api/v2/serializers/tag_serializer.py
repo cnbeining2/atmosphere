bcd3019d6c89bff64eb0b53be1489d7e74a2f8bd
jchansen <jason_hansen@outlook.com>
2015-01-20 15:10:57 -0700

    add user to tags

--- api/v2/serializers/tag_serializer.py
+++ api/v2/serializers/tag_serializer.py
@@ -1,11 +1,17 @@
 from core.models import Tag
 from rest_framework import serializers
+from .user_serializer import UserSerializer
+
 
 class TagSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
-    )
+    # user = serializers.SlugRelatedField(
+    #     read_only=True,
+    #     slug_field='username'
+    # )
+
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    user = UserSerializer()
 
     class Meta:
         model = Tag
